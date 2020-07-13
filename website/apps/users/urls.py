d61e85e6307876fc095fff15b6e248dae71b7f01
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-13 19:36:52 +0200

    Add logout

--- website/apps/users/urls.py
+++ website/apps/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
 from .views import CustomLoginView, HomeView, RegisterView
@@ -6,4 +7,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
