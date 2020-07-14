f937a2adfa04e1b490463afe1f5b668a8b1ffbf9
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-14 16:45:35 +0200

    Add second database for the website

--- website/settings/db.base.py
+++ website/settings/db.base.py
@@ -1,7 +1,15 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'website',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'account',
+        'USER': 'website',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    },
+    'player': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'player',
         'USER': 'website',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
