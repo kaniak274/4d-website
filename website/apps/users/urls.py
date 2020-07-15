57595ed998b9c248ed7d9b9929bd827d5f41abad
Kamil Kucharski <kamil.kucharski@makimo.pl>
2020-07-15 13:40:22 +0200

    Add simple ban page

--- website/apps/users/urls.py
+++ website/apps/users/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from .views import CustomLoginView, HomeView, RegisterView
+from .views import BanListView, CustomLoginView, HomeView, RegisterView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('bans/', BanListView.as_view(), name='ban_list'),
 ]
--- website/apps/users/views.py
+++ website/apps/users/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
-from django.views.generic import CreateView, View
+from django.views.generic import CreateView, ListView, View
 
 from website.apps.news.use_cases import get_all_news
 
@@ -36,3 +36,10 @@ class CustomLoginView(LoginView):
     success_url = '/'
     form_class = AuthenticationForm
     http_method_names = ['post']
+
+
+class BanListView(ListView):
+    model = User
+    queryset = User.objects.filter(banlength__isnull=False)
+    template_name = 'bans/ban_list.html'
+    paginate_by = 20
